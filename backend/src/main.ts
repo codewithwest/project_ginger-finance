5e2c6dcfa0ef11591d690490808bafd6ecc9e4e9
codewithwest <jonaslekgau@gmail.com>
2026-03-11 23:27:24 +0200

    feat: Allow multiple CORS origins from localhost and the `APP_URL` environment variable.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.APP_URL || 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      process.env.APP_URL, // In case it's set to something else
+    ].filter(Boolean) as string[],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
