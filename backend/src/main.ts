619b2db0ea7c391419a98b579b3c3980f8441aa8
codewithwest <jonaslekgau@gmail.com>
2026-03-11 23:11:57 +0200

    config: Configure CORS with dynamic origin, credentials, methods, and allowed headers.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.APP_URL || 'http://localhost:3000',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
